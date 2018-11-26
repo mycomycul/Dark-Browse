bc14303a150edb2f10e0bd38747a3b971c0539c3
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-11-25 22:51:25 -0800

    Adjust Colors

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 document.body.style.backgroundColor = getDarkColor();
-document.body.style.color = "#DDDDDD";
+document.body.style.color = "#FFFFFF";
 
 function getDarkColor() {
     return "#333333";
 }
 
+/*Set to run after all other scripts
+Add navbar button to turn on and off*/
