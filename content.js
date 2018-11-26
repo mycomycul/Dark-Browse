ef40835b11a21049c7a4b26c3f72b730d82a64d5
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-11-25 22:31:08 -0800

    Change to darkbrowse intial

--- content.js
+++ content.js
@@ -1,14 +1,5 @@
+document.body.style.backgroundColor = newFunction();
 
-function insertAfter(el, referenceNode) {
-    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+function newFunction() {
+    return "#999999";
 }
-
-// Create element to inject in page
-var newEl = document.createElement('div');
-newEl.setAttribute("style","-webkit-box-flex: 0;-ms-flex: none;flex: none;width: .5%;background-color: white;")
-//Get element to insert after
-var ref = document.querySelector('#left-container');
-
-insertAfter(newEl, ref);
-
-var x = document.getElementById("left-container'").previousSibling
